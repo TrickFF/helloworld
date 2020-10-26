adb3a376e71f29ecaca498f25b07bab4e6a08007
Trick <mozzhukhin.sergey@gmail.com>
2020-10-26 17:14:02 +0300

    list/in and lesson 2 exercise files added

---
+++ in_ex.py
@@ -0,0 +1,14 @@
+hero = 'Superman'
+
+if hero.find('man') != -1:
+    print('Есть!')
+
+print('man' in hero)
+
+
+# Работа со списком
+
+goals = ['стать гуру языка python', 'здоровье', 'накормить кота']
+
+if 'здоровье' in goals:
+    print('Все норм!')
