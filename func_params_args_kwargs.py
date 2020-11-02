90121c3d21104d4d9c59e617d0c4d6f29e917617
Trick <mozzhukhim.sergey@gmail.com>
2020-11-02 08:07:50 +0300

    func like, param, args/kwargs files changed

--- func_params_args_kwargs.py
+++ func_params_args_kwargs.py
@@ -1,11 +1,16 @@
 def printScores(student, *scores):
-   print(f"Student Name: {student}")
-   for score in scores:
-      print(score)
-printScores("Leo",100, 95, 88, 92, 99)
+    print(f"Student Name: {student}")
+    for score in scores:
+        print(score)
+
+
+printScores("Leo", 100, 95, 88, 92, 99)
+
 
 def printPetNames(owner, **pets):
-   print(f"Owner Name: {owner}")
-   for pet,name in pets.items():
-      print(f"{pet}: {name}")
+    print(f"Owner Name: {owner}")
+    for pet, name in pets.items():
+        print(f"{pet}: {name}")
+
+
 printPetNames("Leo", dog="Brock", fish=["Larry", "Curly", "Moe"], turtle="Shelldon")
